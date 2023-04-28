9b6c4217a18ec219c4b723b5c339a9639dbb4332
Ma Zhiming <101508488+JimmyMa99@users.noreply.github.com>
2023-04-29 03:40:04 +0800

    Update conver_to_coco_format.py

--- label_anything/conver_to_coco_format.py
+++ label_anything/conver_to_coco_format.py
@@ -1,5 +1,19 @@
 import json
 import os
+import argparse
+import numpy as np
+from tqdm import tqdm
+from itertools import groupby
+from label_studio_converter.brush import decode_rle
+
+def parse_args():
+    parser = argparse.ArgumentParser(description='Label studio convert to Coco fomat')
+    parser.add_argument('--json_file_path',default='label_studio.json', help='label studio output json')
+    parser.add_argument('--output_dir',default='coco_format_files', help='output dir of Coco format json')
+
+    args = parser.parse_args()
+    return args
+
 
 coco_format = {}
 images = []
@@ -8,7 +22,34 @@ annotations = []
 bbox = []
 area = []
 
-def format_to_coco(json_file_path):
+def binary_mask_to_rle(binary_mask):
+    rle = {'counts': [], 'size': list(binary_mask.shape)}
+    counts = rle.get('counts')
+    for i, (value, elements) in enumerate(groupby(binary_mask.ravel(order='F'))):
+        if i == 0 and value == 1:
+            counts.append(0)
+        counts.append(len(list(elements)))
+    return rle
+
+def rle2mask(rle,height, width):
+    """Convert RLE to mask
+
+    :param rle: list of ints in RLE format
+    :param height: height of binary mask
+    :param width: width of binary mask
+    :return: binary mask
+    """
+    mask = decode_rle(rle)
+    mask = mask.reshape(-1,4)[:, 0]
+    if mask.max()==255:
+        mask=np.where(mask==255,1,0)
+
+    # mask = mask[:len(mask)//4]
+
+    return mask.reshape((height, width))
+
+def format_to_coco(args):
+    json_file_path=args.json_file_path
     
     # 初始化coco格式的字典
     coco_format = {
@@ -22,20 +63,29 @@ def format_to_coco(json_file_path):
         contents = json.loads(file.read())
 
     # 遍历每个标注
-    for index_annotation in range(len(contents)):
+    for index_annotation in tqdm(range(len(contents))):
         result = contents[index_annotation]['annotations'][0]['was_cancelled']
 
         if result == False:
             width_from_json = contents[index_annotation]["annotations"][0]["result"][0]["original_width"]
             height_from_json = contents[index_annotation]["annotations"][0]["result"][0]["original_height"]
+
+            label = contents[index_annotation]['annotations'][0]['result']
+
+            x = label[0]['value']['x'] * width_from_json / 100
+            y = label[0]['value']['y'] * height_from_json / 100
+            w = label[0]['value']['width'] * width_from_json / 100
+            h = label[0]['value']['height'] * height_from_json / 100
+
             image_id = contents[index_annotation]["id"]
             image_json_name = contents[index_annotation]["data"]['image'].split('/')[-1]
             image_json_name = image_json_name.split('-', 1)[1]
             category = contents[index_annotation]['annotations'][0]['result'][0]['value']['rectanglelabels'][0]
-            organized_pixels_coordenates = contents[index_annotation]['annotations'][0]['result'][1]['value']['rle']
-            label = contents[index_annotation]['annotations'][0]['result']
-            bbox = [label[0]['value']['x'], label[0]['value']['y'], label[0]['value']['width'], label[0]['value']['height']]
-            area = (bbox[2] - bbox[0]) * (bbox[3] - bbox[1])
+            rle = contents[index_annotation]['annotations'][0]['result'][1]['value']['rle']
+            mask=rle2mask(rle,height_from_json,width_from_json)
+            rle=binary_mask_to_rle(mask)
+            bbox = [x, y, w, h]
+            area = w * h
 
             # 添加图像信息到coco格式
             coco_format["images"].append({
@@ -57,7 +107,7 @@ def format_to_coco(json_file_path):
                 "id": index_annotation,
                 "image_id": image_id,
                 "category_id": [d["id"] for d in coco_format["categories"] if d["name"] == category][0],
-                "segmentation": [organized_pixels_coordenates],
+                "segmentation": rle,
                 "bbox": bbox,
                 "area": area,
                 'iscrowd': 0#rle format
@@ -65,60 +115,13 @@ def format_to_coco(json_file_path):
             })
 
     # 将coco格式保存到文件中
-    output_dir="coco_format_files/"
+    output_dir=args.output_dir
     os.makedirs(output_dir, exist_ok=True)
-    with open(f"coco_format_files/coco_file.json", "w") as out_file:
+
+    with open(os.path.join(output_dir,'output.json'), "w") as out_file:
         json.dump(coco_format, out_file, ensure_ascii=False, indent=4)
 
-
-def saving_json(list_data):
-    output_dir="coco_format_files/"
-    os.makedirs(output_dir, exist_ok=True)
-    with open(f"coco_format_files/coco_file.json", "w") as out_file:
-                json.dump(list_data, out_file, ensure_ascii=False, indent=4)
-
-def set_categories(categories):
-
-    set_categories = []
-    list_data = []
-
-    for index in range(len(categories)):
-        set_categories.append(categories[index]['name'])
-
-    index_set = sorted(list(set(set_categories)))
-
-    for index in range(len(index_set)):
-        data = {'id': index, 'name': index_set[index]}  
-        list_data.append(data)
-
-    return list_data
-
-def organizing_coordenates(pixels_coordenates):
-    x_y_coordinates = []
-
-    for index in range(0, len(pixels_coordenates)):
-        x_y_coordinates.append(pixels_coordenates[index][0])
-        x_y_coordinates.append(pixels_coordenates[index][1])
-        
-    return x_y_coordinates
-
-
-def total_area_bounding_box(pixels_coordenates):
-    x_coordinates = []
-    y_coordinates = []
-    result = []
-
-    for index in range(len(pixels_coordenates)):
-
-        x_coordinates.append(pixels_coordenates[index][0])
-        y_coordinates.append(pixels_coordenates[index][1])
-
-    x_min, y_min, x_max, y_max = (min(x_coordinates), min(y_coordinates), max(x_coordinates), max(y_coordinates))
-    area_segmentation = (x_max - x_min) * (y_max - y_min)
-    result.append([x_min, y_min, x_max, y_max, area_segmentation])
-    
-    return result
- 
-format_to_coco('label_studio.json')
-
-    
+if __name__ == '__main__':
+    args = parse_args()
+    format_to_coco(args)
+   
