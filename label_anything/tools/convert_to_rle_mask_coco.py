6a60027ee4926819aef07b1e3782de10a85b82a1
Ma Zhiming <101508488+JimmyMa99@users.noreply.github.com>
2023-05-06 10:27:07 +0800

    Update convert_to_rle_mask_coco.py

--- label_anything/tools/convert_to_rle_mask_coco.py
+++ label_anything/tools/convert_to_rle_mask_coco.py
@@ -14,10 +14,10 @@ import shutil
 
 def parse_args():
     parser = argparse.ArgumentParser(description='Label studio convert to Coco fomat')
-    parser.add_argument('--json_file_path',default='project.json', help='label studio output json')
+    parser.add_argument('--json_file_path',default='/media/ders/mazhiming/mm/project-4.json', help='label studio output json')
     parser.add_argument('--out_dir',default='../mmdetection/data/my_set', help='output dir of Coco format json')
     parser.add_argument('--classes',default=None, help='Classes list of the dataset, if None please check the output.')
-    parser.add_argument('--out_config',default='rtmdet-ins_s', choices=['mask-rcnn_r50_fpn','rtmdet_l','rtmdet-ins_s','rtmdet_s',None],help='config mode')
+    parser.add_argument('--out_config',default='rtmdet-ins_s', choices=['mask-rcnn_r50_fpn','rtmdet-ins_s',None],help='config mode')
 
     args = parser.parse_args()
     return args
@@ -164,15 +164,9 @@ def format_to_coco(args):
     return classes_output,args
 
 def move_to_cfg(args,classes_list):
-    if 'rtmdet_l' in args.out_config:
-        config_path='config_template/rtmdet_l_syncbn_fast_8xb32-300e_coco.py'
-        config_name='rtmdet_l_syncbn_fast_8xb32.py'
-    elif 'rtmdet-ins_s' in args.out_config:
+    if 'rtmdet-ins_s' in args.out_config:
         config_path='config_template/rtmdet-ins_s_8xb32-300e_coco.py'
         config_name='rtmdet-ins_s.py'
-    elif 'rtmdet_s' in args.out_config:
-        config_path='config_template/rtmdet_s_syncbn_fast_8xb32-300e_coco.py'
-        config_name='rtmdet_s_syncbn_fast_8xb32.py'
     elif 'mask-rcnn_r50_fpn' in args.out_config:
         config_path='config_template/mask-rcnn_r50_fpn_1x_coco.py'
         config_name='mask-rcnn_r50_fpn.py'
