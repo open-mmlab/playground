3e59c0e452a30c91d1060a9b5a7ccea29d92cba6
geoffreyfan <105597268+geoffreyfan@users.noreply.github.com>
2023-07-14 14:56:05 +0800

    Update is_there_tensorboard.py

--- cvmart/Helmet_identification_10163/is_there_tensorboard.py
+++ cvmart/Helmet_identification_10163/is_there_tensorboard.py
@@ -1,4 +1,5 @@
 import os
+
 if os.path.exists('/project/train/tensorboard'):
     # os.system('cp -r /project/train/models/train/exp/weights/* /project/train/tensorboard/')
     os.system('rm -rf /project/train/tensorboard')
